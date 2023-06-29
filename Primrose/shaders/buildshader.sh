482223c59e27bfaccf21e67ee5c54246c83ce4fe
Ryan Schneider <u7509728@anu.edu.au>
2023-06-30 01:21:50 +1000

    cmake yayyyyy

--- Primrose/shaders/buildshader.sh
+++ Primrose/shaders/buildshader.sh
@@ -12,15 +12,25 @@ if [[ $? -ne 0 ]] ; then
   exit 1
 fi
 
+#echo "#ifndef ${name^^}_${stage^^}_SPV_H
+##define ${name^^}_${stage^^}_SPV_H
+##include <stdint.h>
+#extern uint8_t $name${stage^}SpvData[];
+#extern size_t $name${stage^}SpvSize;
+##endif" > "$dir/../embed/${name}_${stage}_spv.h"
+#
+#echo "#include \"${name}_${stage}_spv.h\"
+#uint8_t $name${stage^}SpvData[] = {" > "$dir/../embed/${name}_${stage}_spv.c"
+#xxd -plain "$dir/../embed/${name}_$stage.spv" | sed 's/\(.\{2\}\)/0x\1,/g' >> "$dir/../embed/${name}_${stage}_spv.c"
+#echo "};
+#size_t $name${stage^}SpvSize = sizeof($name${stage^}SpvData);" >> "$dir/../embed/${name}_${stage}_spv.c"
+
 echo "#ifndef ${name^^}_${stage^^}_SPV_H
 #define ${name^^}_${stage^^}_SPV_H
-#include <stdint.h>
-extern uint8_t $name${stage^}SpvData[];
-extern size_t $name${stage^}SpvSize;
-#endif" > "$dir/../embed/${name}_${stage}_spv.h"
-
-echo "#include \"${name}_${stage}_spv.h\"
-uint8_t $name${stage^}SpvData[] = {" > "$dir/../embed/${name}_${stage}_spv.c"
-xxd -plain "$dir/../embed/${name}_$stage.spv" | sed 's/\(.\{2\}\)/0x\1,/g' >> "$dir/../embed/${name}_${stage}_spv.c"
+uint8_t $name${stage^}SpvData[] = {" > "$dir/../embed/${name}_${stage}_spv.h"
+xxd -plain "$dir/../embed/${name}_$stage.spv" | sed 's/\(.\{2\}\)/0x\1,/g' >> "$dir/../embed/${name}_${stage}_spv.h"
 echo "};
-size_t $name${stage^}SpvSize = sizeof($name${stage^}SpvData);" >> "$dir/../embed/${name}_${stage}_spv.c"
+size_t $name${stage^}SpvSize = sizeof($name${stage^}SpvData);
+#endif" >> "$dir/../embed/${name}_${stage}_spv.h"
+
+rm "$dir/../embed/${name}_$stage.spv"
